7c05d3451161caea1c6782184d5341b17be6f4ae
kingsuk <work.kingsuk@gmail.com>
2024-11-12 11:14:50 +0530

    add dns message Header

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,16 @@
 import * as dgram from "dgram";
+import { DnsMessageHeader } from "./dnsMessage";
 
 const udpSocket: dgram.Socket = dgram.createSocket("udp4");
+
+const defeaultHeader = new DnsMessageHeader();
+defeaultHeader.packetID = 1234;
+defeaultHeader.isResponse = true;
 
 udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
   try {
     console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
-    const response = Buffer.from("");
+    const response = Buffer.from(defeaultHeader.encode());
     udpSocket.send(response, remoteAddr.port, remoteAddr.address);
   } catch (e) {
     console.log(`Error sending data: ${e}`);
