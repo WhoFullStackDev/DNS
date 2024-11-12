61d85a9c2ca2b85951cb0c3feacc6abf7a43b6ab
kingsuk <work.kingsuk@gmail.com>
2024-11-12 11:55:17 +0530

    add dns qustion

--- app/main.ts
+++ app/main.ts
@@ -1,28 +1,88 @@
 import * as dgram from "dgram";
-import { DnsMessageHeader } from "./dnsMessage";
-import type { Question } from "./writeQuestion";
-import writeHeader from "./writeaHeader";
 
 const udpSocket: dgram.Socket = dgram.createSocket("udp4");
+udpSocket.bind(2053, "127.0.0.1");
 
-const defeaultHeader = new DnsMessageHeader();
-defeaultHeader.packetID = 1234;
-defeaultHeader.isResponse = true;
-
+const constructHeader = ({
+  QR = 0,
+  TC = 0,
+  RD = 0,
+  Z = 0,
+  RCODE = 0,
+  OPCODE = 0,
+  AA = 0,
+  RA = 0,
+  NSCOUNT = 0,
+  ARCOUNT = 0,
+  ANCOUNT = 0,
+  QDCOUNT = 0,
+  packetId = 0,
+}: {
+  packetId?: number; // 16 bits
+  QR?: number; // 1 bit
+  OPCODE?: number; // 4 bits
+  AA?: number; // 1 bit
+  TC?: number; // 1 bit
+  RD?: number; // 1 bit
+  RA?: number; // 1 bit
+  Z?: number; // 3 bits
+  RCODE?: number; // 4 bits
+  QDCOUNT?: number; // 16 bits
+  ANCOUNT?: number; // 16 bits
+  NSCOUNT?: number; // 16 bits
+  ARCOUNT?: number; // 16 bits
+}) => {
+  const buffer = Buffer.alloc(12);
+  buffer.writeInt16BE(packetId);
+  const computed =
+    (QR << 15) +
+    (OPCODE << 11) +
+    (AA << 10) +
+    (TC << 9) +
+    (RD << 8) +
+    (RA << 7) +
+    (Z << 4) +
+    RCODE;
+  buffer.writeInt16BE(computed, 2);
+  buffer.writeInt16BE(QDCOUNT, 4);
+  buffer.writeInt16BE(ANCOUNT, 6);
+  buffer.writeInt16BE(NSCOUNT, 8);
+  buffer.writeInt16BE(ARCOUNT, 10);
+  return buffer;
+};
+type Question = {
+  type: number;
+  class: number;
+  domainName: string;
+};
+const writeQuestions = (questions: Question[]) => {
+  return Buffer.concat(
+    questions.map((q) => {
+      const typeAndClass = Buffer.alloc(4);
+      const s = q.domainName
+        .split(".")
+        .map((e) => `${String.fromCharCode(e.length)}${e}`)
+        .join("");
+      typeAndClass.writeInt16BE(q.type);
+      typeAndClass.writeInt16BE(q.class, 2);
+      return Buffer.concat([Buffer.from(s + "\0", "binary"), typeAndClass]);
+    })
+  );
+};
 udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
   try {
     console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
+
     const questions: Question[] = [
-      { domainName: "codecrafters.io", class: 1, type: 1 },
+      { class: 1, type: 1, domainName: "codecrafters.io" },
     ];
-    const header = writeHeader({
+    const header = constructHeader({
       packetId: 1234,
       QR: 1,
       QDCOUNT: questions.length,
     });
     udpSocket.send(
-      // @ts-expect-error
-      Buffer.concat([header, writeHeader(questions)]),
+      Buffer.concat([header, writeQuestions(questions)]),
       remoteAddr.port,
       remoteAddr.address
     );
@@ -30,5 +90,3 @@ udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
     console.log(`Error sending data: ${e}`);
   }
 });
-
-udpSocket.bind(2053, "127.0.0.1");
